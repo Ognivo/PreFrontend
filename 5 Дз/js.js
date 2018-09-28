1286ee3998ac1d4a35420048cf2f97fbc334d4d0
Nikita <1588820@mail.ru>
2018-09-29 01:47:53 +0300

    4 ready

---
+++ 5 Дз/js.js
@@ -0,0 +1,9 @@
+function knopka1(){
+    document.location.href = "https://github.com/";
+};
+function knopka2(){
+    document.location.href = "https://github.com/";
+};
+function knopka3(){
+    document.location.href = "https://github.com/";
+};
